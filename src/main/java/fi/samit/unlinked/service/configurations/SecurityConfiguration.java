301dbb94ef549b23c13033f8221c6b3d5c796499
Erkki Rekimies <sami.tuppurainen@metropolia.fi>
2021-07-07 00:10:51 +0300

    Moved MvcConfig to own class

--- src/main/java/fi/samit/unlinked/service/configurations/SecurityConfiguration.java
+++ src/main/java/fi/samit/unlinked/service/configurations/SecurityConfiguration.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -53,17 +51,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Configuration
-    public class MvcConfig implements WebMvcConfigurer {
-
-        @Override
-        public void addViewControllers(ViewControllerRegistry registry) {
-
-            registry.addViewController("/login").setViewName("login");
-
-        }
-
     }
 }
